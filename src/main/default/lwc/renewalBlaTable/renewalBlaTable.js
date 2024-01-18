a6e16ca48191695d204b20eb13ed2fbfea58bd65
Keerthana9894 <k.t.srinivasan@accenture.com>
2024-01-18 16:48:09 +0530

    ALR-795

--- src/main/default/lwc/renewalBlaTable/renewalBlaTable.js
+++ src/main/default/lwc/renewalBlaTable/renewalBlaTable.js
@@ -7,16 +7,15 @@ import { refreshApex } from "@salesforce/apex";
 const tableColumns = [
 {label: 'Application Id', fieldName: 'appId', type: 'url',
 typeAttributes: {label: { fieldName: 'Name' }, target: '_parent'}},
-{label: 'Application Type', fieldName: 'ApplicationType', type: 'text'},
+{label: 'License Type', fieldName: 'LicenseTypeId', type: 'url',
+  typeAttributes: {label: { fieldName: 'LicenseTypeName'}, target: '_parent'}},
 {label: 'Renewal Year', fieldName: 'RenewalYear__c', type: 'text'},
-{label: 'Residence Name2', fieldName: 'AccId', type: 'url',
+{label: 'Residence Name', fieldName: 'AccId', type: 'url',
     typeAttributes: {label: { fieldName: 'AccName' }, target: '_parent'}},
 {label: 'Parent Account', fieldName: 'ParentId', type: 'url',
     typeAttributes: {label: { fieldName: 'ParentName' }, target: '_parent'}},
 {label: 'Health Authority', fieldName: 'HealthAuthorityName', type: 'text'},
-{label: 'Class Type', fieldName: 'LicenseTypeId', type: 'url',
-  typeAttributes: {label: { fieldName: 'LicenseTypeName'}, target: '_parent'}},
-{label: 'Status', fieldName: 'AccountStatus', type: 'text'},
+{label: 'Residence Status', fieldName: 'AccountStatus', type: 'text'},
 {label: 'Application Status', fieldName: 'Status', type: 'text', editable: true},
 {label: 'Exclusion Reason', fieldName: 'ExclusionReason__c', type: 'text', editable: true
  , cellAttributes: { alignment: 'left' }}
@@ -28,6 +27,7 @@ export default class RenewalBlaTable extends LightningElement {
     @track columns = tableColumns;
     @track blaList;
     draftValues = [];
+    @track hasLoaded = false;//spinner attribute
     //wired property
     _wiredResult;
 
@@ -39,12 +39,10 @@ export default class RenewalBlaTable extends LightningElement {
             const data = result.data;
             let blaParsedData = JSON.parse(JSON.stringify(data));
             blaParsedData.forEach(bla => {
-                console.log('dcs  data '+bla);
                 if(bla.Name) {
                     bla.appId = '/'+bla.Id;
                 }
                 if(bla.Account.Id) {
-                    console.log('dcs  inside '+bla);
                     bla.AccName = bla.Account.Name;
                     bla.AccId = '/'+bla.Account.Id;
                     bla.ParentName = bla.Account.Parent.Name;
@@ -57,8 +55,8 @@ export default class RenewalBlaTable extends LightningElement {
                     bla.LicenseTypeId = '/'+bla.LicenseTypeId;
                 }
             });
-            console.log('vsfdx'+blaParsedData);
             this.blaList = blaParsedData;
+            this.hasLoaded = true;//to remove spinnner
             this.error = undefined;
         } else if(result.error) {
             this.error = result.error;
@@ -68,19 +66,19 @@ export default class RenewalBlaTable extends LightningElement {
 
     //on click of save
     async handleSave(event) {
-        const saveDraftValues = event.detail.draftValues;
-        console.log('cdscd s'+saveDraftValues+' '+event.detail.draftValues);
-        console.log('cdscd s'+JSON.stringify(saveDraftValues)+' '+JSON.stringify(event.detail.draftValues));
         try {
-            // Pass edited fields to the Apex controller
-            await updateBlaRecs({ data: saveDraftValues });
-            //refresh the table with updated data
-            await this.refreshData();
-            this.draftValues = [];
+            if(event.detail.draftValues) {
+                this.hasLoaded = false;
+                const saveDraftValues = event.detail.draftValues;
+                // Pass edited fields to the Apex controller
+                await updateBlaRecs({ data: saveDraftValues });
+                //refresh the table with updated data
+                await this.refreshData();
+                this.draftValues = [];
+            }
         } catch (error) {
             let message = error.body.message.includes('FIELD_CUSTOM_VALIDATION_EXCEPTION') ?
                 error.body.message.split('EXCEPTION, ')[1].split(': [')[0] : error.body.message;
-            //this.showToastMessage('Error', message, 'error');
         };
     }
 
@@ -90,7 +88,15 @@ export default class RenewalBlaTable extends LightningElement {
     }
 
     //send renewals on click
-    handleSendRenewals(event) {
-        sendRenewals();
+    async handleSendRenewals(event) {
+        try {
+            this.hasLoaded = false;
+            await sendRenewals();
+            await this.refreshData();
+        } catch (error) {
+            let message = error.body.message.includes('FIELD_CUSTOM_VALIDATION_EXCEPTION') ?
+                error.body.message.split('EXCEPTION, ')[1].split(': [')[0] : error.body.message;
+                //this.showToastMessage('Error', message, 'error');
+        };
     }
 }
